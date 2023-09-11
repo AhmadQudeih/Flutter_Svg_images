b9700cd20d9ae085f1239a3ddbf86aefbd0531a6
Ahmad Qudeih <ahmadbfqudeih@gmail.com>
2023-09-11 20:18:27 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
@@ -105,6 +112,6 @@ void main() {
               ]),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
